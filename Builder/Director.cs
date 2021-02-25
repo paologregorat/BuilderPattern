a65a8f82b113cc5f34e8c74acfcdcb1809d71647
Paolo Gregorat <paolo.gregorat@henshingroup.com>
2021-02-25 11:37:32 +0100

    first commit

---
+++ Builder/Director.cs
@@ -0,0 +1,36 @@
+﻿using BuilderPattern.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuilderPattern.Builder
+{
+    // The Director is only responsible for executing the building steps in a
+    // particular sequence. It is helpful when producing products according to a
+    // specific order or configuration. Strictly speaking, the Director class is
+    // optional, since the client can control builders directly.
+    public class Director
+    {
+        private IBuilder _builder;
+
+        public IBuilder Builder
+        {
+            set { _builder = value; }
+        }
+
+        // The Director can construct several product variations using the same
+        // building steps.
+        public void BuildMinimalViableProduct()
+        {
+            _builder.BuildPartA();
+        }
+
+        public void BuildFullFeaturedProduct()
+        {
+            _builder.BuildPartA();
+            _builder.BuildPartB();
+            _builder.BuildPartC();
+        }
+    }
+}
